a40d844e3709e1463e82381f8ec24089465e5061
dannyzou18 <dzou3814@gmail.com>
2024-06-04 16:24:41 -0400

    fixxed an bug

--- lifescape-app/app/(tabs)/_layout.tsx
+++ lifescape-app/app/(tabs)/_layout.tsx
@@ -27,7 +27,7 @@ export default function TabLayout() {
         headerShown: useClientOnlyValue(false, true),
       }}>
       <Tabs.Screen
-        name="home"
+        name="index"
         options={{
           title: 'Home',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
