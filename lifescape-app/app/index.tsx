f1c9b6d23a95554736983a0fb2e06d40438e9ee1
dannyzou18 <dzou3814@gmail.com>
2024-06-04 15:02:18 -0400

    started express app

---
+++ lifescape-app/app/index.tsx
@@ -0,0 +1,16 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Home</Text>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
---
+++ lifescape-server/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
