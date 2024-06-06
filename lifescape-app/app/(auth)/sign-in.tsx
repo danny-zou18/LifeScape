89af6f2eb216f79cbf81839de648f18e15944c93
dannyzou18 <dzou3814@gmail.com>
2024-06-06 12:29:29 -0400

    work on mac

--- lifescape-app/app/(auth)/sign-in.tsx
+++ lifescape-app/app/(auth)/sign-in.tsx
@@ -1,21 +1,33 @@
-import { View, Text, SafeAreaView, ScrollView } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, ScrollView, TextInput } from "react-native";
+import React, { useState } from "react";
 
-import { Link } from 'expo-router'
+import { FIREBASE_AUTH } from "@/FirebaseConfig";
+
+import { Link } from "expo-router";
 
 const signIn = () => {
+  const [email_or_username, set_email_or_username] = useState("");
+  const [password, set_password] = useState("");
+  const [loading, set_loading] = useState(false);
+
+  const auth = FIREBASE_AUTH;
+
   return (
-    <SafeAreaView className='h-full'>
+    <SafeAreaView className="h-full">
       <ScrollView>
-        <View className='w-full flex justify-center min-h-[85vh] px-4 my-6'>
-          <View className='flex flex-row'>
+        <View className="w-full min-h-[85vh]  flex flex-col justify-center items-center px-4 my-6">
+          <TextInput placeholder="Email Or Username"></TextInput>
+          <TextInput placeholder="Password"></TextInput>
+          <View className="flex flex-row">
             <Text>Don't have an Account?</Text>
-            <Link href="/sign-up" className='ml-4'>Sign up</Link>
+            <Link href="/sign-up" className="ml-4">
+              Sign up
+            </Link>
           </View>
         </View>
       </ScrollView>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default signIn
+export default signIn;
--- lifescape-app/app/index.tsx
+++ lifescape-app/app/index.tsx
@@ -11,7 +11,7 @@ const index = () => {
   return (
     <SafeAreaView >
       <ScrollView contentContainerStyle={{ height: "100%" }}>
-        <View className="w-full flex-1 justify-center items-center h-full px-4">
+        <View className="w-full min-h-[100vh] flex-1 justify-center items-center  px-4 bg-white">
           <Text className="">HELLO THIS IS THE ONBOARDING PAGE</Text>
           <CustomButton title="Sign in" onPress={()=>router.push("/sign-in") } />
         </View>
