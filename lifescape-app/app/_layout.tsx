c9f7a82294685e0f6bfc0e6c1e33a598e44d5d25
Ramzey <143454641+youssefr26@users.noreply.github.com>
2024-06-07 13:37:04 -0400

    Start login page

--- lifescape-app/app/_layout.tsx
+++ lifescape-app/app/_layout.tsx
@@ -53,6 +53,7 @@ function RootLayoutNav() {
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+        <Stack.Screen name="login" options={{ presentation: 'login'}} />
       </Stack>
     </ThemeProvider>
   );
