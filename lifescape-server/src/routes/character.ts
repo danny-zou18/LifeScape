c2aeee07c88c0e20e6971b13145c5d81d6a480c5
dannyzou18 <dzou3814@gmail.com>
2024-06-09 15:07:13 -0400

    added character creation endpoint

--- lifescape-server/src/routes/character.ts
+++ lifescape-server/src/routes/character.ts
@@ -5,41 +5,88 @@ import { auth } from "firebase-admin";
 
 const router = express.Router();
 
-router.get("/:userId", async (req, res) => {
-    const authToken = req.headers.authorization;
-    const { userId } = req.params;
-    console.log(userId);
-    console.log(authToken);
-    try {
-        const authUser = await auth().verifyIdToken(authToken as string);
-        if (authUser.uid !== userId) {
-            res.status(403).json({error: "Unauthorized"});
-        }
-    } catch (e) {
-        return res.status(401).json({error: "Unauthorized"});
+router.get("/get/:userId", async (req, res) => {
+  const authToken = req.headers.authorization;
+  const { userId } = req.params;
+  console.log(userId);
+  console.log(authToken);
+  try {
+    const authUser = await auth().verifyIdToken(authToken as string);
+    if (authUser.uid !== userId) {
+      res.status(403).json({ error: "Unauthorized" });
     }
+  } catch (e) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
 
-    try {
-        const userWithCharacter = await db.users.findUnique({
-            where: { id: userId },
-            include: { character: true },
-        })
-        if (!userWithCharacter) {
-            return res.status(404).json({ error: "User not found" });
-        }
-        const character = userWithCharacter.character;
-        if (!character) {
-            return res.json(null);
-        }
-        res.json(character);
-    } catch (e) {
-        console.log(e);
-        return  res.status(500).json({ error: "Internal Server Error" });
+  try {
+    const userWithCharacter = await db.users.findUnique({
+      where: { id: userId },
+      include: { character: true },
+    });
+    if (!userWithCharacter) {
+      return res.status(404).json({ error: "User not found" });
     }
+    const character = userWithCharacter.character;
+    if (!character) {
+      return res.json(null);
+    }
+    res.json(character);
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
+router.post("/create/:userId", async (req, res) => {
+  const authToken = req.headers.authorization;
+  const { userId } = req.params;
+  
+  //Authorize
+  try {
+    const authUser = await auth().verifyIdToken(authToken as string);
+    if (authUser.uid !== userId) {
+      res.status(403).json({ error: "Unauthorized" });
+    }
+  } catch (e) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
 
+  //Check if user already has a character
+  try {
+    const userWithCharacter = await db.users.findUnique({
+      where: { id: userId },
+      include: { character: true },
+    });
+    if (!userWithCharacter) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    const character = userWithCharacter.character;
+    if (character) {
+      return res.status(404).json({ error: "User already has a character" });
+    }
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
 
+  const { name } = req.body;
 
-})
+  //Create the character
+  await db.character
+    .create({
+      data: {
+        name: name,
+        OwnerId: userId,
+      },
+    })
+    .then((character) => {
+      return res.json({ success: true, character: character });
+    })
+    .catch((error) => {
+      console.log(error);
+      return res.status(500).json({ error: "Internal Server Error" });
+    });
+});
 
 export { router as CharacterRouter };
