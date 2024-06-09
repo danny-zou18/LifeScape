1fbc5416ad1a15711a2928b5c55b5d099781fb3a
dannyzou18 <dzou3814@gmail.com>
2024-06-09 14:10:31 -0400

    created character creation modal

--- lifescape-app/app/(app)/(tabs)/home.tsx
+++ lifescape-app/app/(app)/(tabs)/home.tsx
@@ -1,15 +1,56 @@
-import { View, Text } from 'react-native'
+import {
+  View,
+  Text,
+  TouchableHighlight,
+  Button,
+  Modal,
+  SafeAreaView,
+} from "react-native";
 
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
-import axios from 'axios'
+import { useGlobalContext } from "@/context/GlobalProvider";
 
 const Home = () => {
-  return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  )
-}
+  const [loading, setLoading] = useState<boolean>(false);
+  const [characterCreationModalVisible, setCharacterCreationModalVisible] =
+    useState<boolean>(false);
 
-export default Home
+  const { userCharacter, setUserCharacter } = useGlobalContext();
+
+  return (
+    <SafeAreaView>
+      {userCharacter ? (
+        <Text>{userCharacter.name}</Text>
+      ) : (
+        <View>
+          <Text>Plase make a character</Text>
+          <TouchableHighlight
+            onPress={() => setCharacterCreationModalVisible(true)}
+            className="bg-[#FDFDFD] w-[225px] h-[45px] rounded-md mt-4"
+            underlayColor="#FFFFFF"
+          >
+            <Text className="text-black text-xl font-semibold mx-auto my-auto">
+              Make your Character
+            </Text>
+          </TouchableHighlight>
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={characterCreationModalVisible}
+          >
+            <SafeAreaView className="bg-red-400 h-full p-20">
+              <Text>Hello</Text>
+              <Button
+                title="close"
+                onPress={() => setCharacterCreationModalVisible(false)}
+              ></Button>
+            </SafeAreaView>
+          </Modal>
+        </View>
+      )}
+    </SafeAreaView>
+  );
+};
+
+export default Home;
