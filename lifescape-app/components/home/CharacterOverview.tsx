bdf9cade4b72d8713281499efd29cc01ddccbbd2
dannyzou18 <dzou3814@gmail.com>
2024-06-10 16:06:46 -0400

    layout of essential stats home page

--- lifescape-app/components/home/CharacterOverview.tsx
+++ lifescape-app/components/home/CharacterOverview.tsx
@@ -8,13 +8,19 @@ import cat from "../../constants/Images.ts";
 const CharacterOverview = () => {
   const { userCharacter } = useGlobalContext();
 
-  const calculateExpPercentage = (current: number, nextLevel: number): string => {
-    if (nextLevel === 0) return '0%';
+  const calculateExpPercentage = (
+    current: number,
+    nextLevel: number
+  ): string => {
+    if (nextLevel === 0) return "0%";
     const percentage = (current / nextLevel) * 100;
     return `${percentage}%`;
   };
 
-  const expPercentage = useMemo(() => calculateExpPercentage(userCharacter.experience, 20), [userCharacter.experience]);
+  const expPercentage = useMemo(
+    () => calculateExpPercentage(userCharacter.experience, 20),
+    [userCharacter.experience]
+  );
 
   return (
     <View className="bg-red-300 flex flex-row h-[17vh] p-4 pl-5">
@@ -41,8 +47,23 @@ const CharacterOverview = () => {
             <Text className="text-xs ">{userCharacter.level}</Text>
           </View>
           <View className="bg-red-50 ml-1 mt-[0.15rem] min-w-[76%] max-h-[70%] rounded-full overflow-hidden">
-            <View style={{width: expPercentage}} className={`bg-green-400 h-full rounded-l-full rounded-r-lg flex items-center justify-center`}>
-            </View>
+            <View
+              style={{ width: expPercentage }}
+              className={`bg-green-400 h-full rounded-l-full rounded-r-lg flex items-center justify-center`}
+            ></View>
+          </View>
+        </View>
+      </View>
+      <View className="ml-2 w-full bg-blue-200 flex flex-col">
+        <View className="bg-red-200 p-1 h-[85%]">
+          <View>
+            <Text>Health</Text>
+          </View>
+          <View>
+            <Text>Health</Text>
+          </View>
+          <View>
+            <Text>Health</Text>
           </View>
         </View>
       </View>
@@ -50,6 +71,5 @@ const CharacterOverview = () => {
   );
 };
 //max-w-[${1 / 10 * 100}%]
-
 
 export default CharacterOverview;
