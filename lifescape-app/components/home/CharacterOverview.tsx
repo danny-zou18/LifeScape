4d0b5645af33ae910f7cb445f27f4fc806af1978
dannyzou18 <dzou3814@gmail.com>
2024-06-10 15:25:43 -0400

    got xp percent bar to work

--- lifescape-app/components/home/CharacterOverview.tsx
+++ lifescape-app/components/home/CharacterOverview.tsx
@@ -1,5 +1,5 @@
-import { View, Text, Image } from "react-native";
-import React from "react";
+import { View, Text, Image, StyleSheet } from "react-native";
+import React, { useMemo, useEffect } from "react";
 
 import { useGlobalContext } from "@/context/GlobalProvider";
 
@@ -7,6 +7,15 @@ import cat from "../../constants/Images.ts";
 
 const CharacterOverview = () => {
   const { userCharacter } = useGlobalContext();
+
+  const calculateExpPercentage = (current: number, nextLevel: number): string => {
+    if (nextLevel === 0) return '0%';
+    const percentage = (current / nextLevel) * 100;
+    return `${percentage}%`;
+  };
+
+  const expPercentage = useMemo(() => calculateExpPercentage(userCharacter.experience, 20), [userCharacter.experience]);
+
   return (
     <View className="bg-red-300 flex flex-row h-[17vh] p-4 pl-5">
       <View className="w-[35%] h-[95%] relative mt-auto mb-auto">
@@ -19,13 +28,20 @@ const CharacterOverview = () => {
             uri: "https://media1.tenor.com/m/sepKXIenuG0AAAAC/cat-meme-cat-meme-face.gif",
           }}
         />
-        <View className={`absolute bottom-[-.5rem] z-10 bg-transparent flex flex-row ${userCharacter.experience >= 10 ? 'left-0' : 'left-1'}`}>
+        <View
+          className={`absolute bottom-[-.5rem] z-10 bg-transparent flex flex-row ${
+            userCharacter.experience >= 100
+              ? "left-[-1rem]"
+              : userCharacter.experience >= 10
+              ? "left-0"
+              : "left-1"
+          }`}
+        >
           <View className="bg-red-50 px-[0.4rem] rounded-full">
             <Text className="text-xs ">{userCharacter.level}</Text>
           </View>
-          <View className="bg-red-50 ml-1 mt-[0.06rem] min-w-[76%] max-h-[90%] rounded-full ">
-            <View className="bg-green-50 min-w-[50%] h-full rounded-full flex items-center justify-center">
-              <Text className="text-xs">{userCharacter.experience}</Text>
+          <View className="bg-red-50 ml-1 mt-[0.15rem] min-w-[76%] max-h-[70%] rounded-full overflow-hidden">
+            <View style={{width: expPercentage}} className={`bg-green-400 h-full rounded-l-full rounded-r-lg flex items-center justify-center`}>
             </View>
           </View>
         </View>
@@ -33,5 +49,7 @@ const CharacterOverview = () => {
     </View>
   );
 };
+//max-w-[${1 / 10 * 100}%]
+
 
 export default CharacterOverview;
