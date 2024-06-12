63c4a7c6fff98ce017ca7f9a76c71a72f9098ba5
dannyzou18 <dzou3814@gmail.com>
2024-06-12 19:40:10 -0400

    implemented task deletion endpoint

--- lifescape-server/src/routes/tasks.ts
+++ lifescape-server/src/routes/tasks.ts
@@ -53,16 +53,46 @@ router.get("/get/:userId/:characterId", async (req, res) => {
     return res.status(401).json({ error: "Unauthorized" });
   }
 
-  await db.task.findMany({
-    where: {
-      CharacterId: parseInt(characterId),
-    },
-  }).then((tasks) => {
-    return res.status(200).json(tasks);
-  }).catch((error) => {
-    console.log(error);
-    return res.status(500).json({ error: "Internal Server Error" });
-  });
+  await db.task
+    .findMany({
+      where: {
+        CharacterId: parseInt(characterId),
+      },
+    })
+    .then((tasks) => {
+      return res.status(200).json(tasks);
+    })
+    .catch((error) => {
+      console.log(error);
+      return res.status(500).json({ error: "Internal Server Error" });
+    });
 });
 
 export { router as tasksRouter };
+
+router.post("/delete/:userId/:characterId/:taskId", async (req, res) => {
+  const authToken = req.headers.authorization;
+  const { userId, characterId, taskId } = req.params;
+  try {
+    const authUser = await auth().verifyIdToken(authToken as string);
+    if (authUser.uid !== userId) {
+      res.status(403).json({ error: "Unauthorized" });
+    }
+  } catch (e) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+
+  await db.task
+    .delete({
+      where: {
+        id: parseInt(taskId),
+      },
+    })
+    .then(() => {
+      return res.status(200).json({ message: "Task deleted successfully" });
+    })
+    .catch((error) => {
+      console.log(error);
+      return res.status(500).json({ error: "Internal Server Error" });
+    });
+});
