eedb3388136c06d97ed7980c0693575fa9274fce
dannyzou18 <dzou3814@gmail.com>
2024-06-11 21:24:59 -0400

    added task creation modal

--- lifescape-app/components/home/TaskWrapper.tsx
+++ lifescape-app/components/home/TaskWrapper.tsx
@@ -4,20 +4,23 @@ import React, { useState } from "react";
 import TaskCreationModal from "../tasks/TaskCreationModal";
 
 const TaskWrapper: React.FC = () => {
-    const [taskCreationOpen, setTaskCreationOpen] = useState<boolean>(false);
+  const [taskCreationOpen, setTaskCreationOpen] = useState<boolean>(false);
 
   return (
     <View>
+      <TaskCreationModal
+        isOpen={taskCreationOpen}
+        setOpen={setTaskCreationOpen}
+      />
       <TouchableHighlight
         className="bg-[#FDFDFD] w-[225px] h-[45px] rounded-md mt-4"
         underlayColor="#FFFFFF"
-        // onPress={() => handle_creation()}
+        onPress={() => setTaskCreationOpen(true)}
       >
         <Text className="text-black text-xl font-semibold mx-auto my-auto">
           Create Task
         </Text>
       </TouchableHighlight>
-        
     </View>
   );
 };
--- lifescape-app/components/tasks/TaskCreationModal.tsx
+++ lifescape-app/components/tasks/TaskCreationModal.tsx
@@ -1,17 +1,31 @@
-import { View, Text, Modal, TouchableHighlight } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  Modal,
+  TouchableHighlight,
+  Button,
+  SafeAreaView,
+} from "react-native";
+import React from "react";
+import { set } from "react-hook-form";
 
 interface TaskCreationModalProps {
-    isOpen: boolean;
-    setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  isOpen: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const TaskCreationModal = () => {
+const TaskCreationModal: React.FC<TaskCreationModalProps> = ({
+  isOpen,
+  setOpen,
+}) => {
   return (
-    <Modal>
-      <Text>TaskCreationModal</Text>
+    <Modal animationType="slide" visible={isOpen}>
+      <SafeAreaView>
+        <Text>TaskCreationModal</Text>
+        <Button title="Cancel" onPress={() => setOpen(false)}></Button>
+      </SafeAreaView>
     </Modal>
-  )
-}
+  );
+};
 
-export default TaskCreationModal
+export default TaskCreationModal;
