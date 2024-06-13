e26cbc3106576959d5b6c02bb20d63a5130bfd66
dannyzou18 <dzou3814@gmail.com>
2024-06-13 17:24:17 -0400

    view selection btns work

--- lifescape-app/components/home/ViewSelectionBtns.tsx
+++ lifescape-app/components/home/ViewSelectionBtns.tsx
@@ -6,19 +6,59 @@ interface ViewSelectionBtnsProps {
   setCurrentView: React.Dispatch<React.SetStateAction<string>>;
 }
 
-const ViewSelectionBtns: React.FC<ViewSelectionBtnsProps> = () => {
+const ViewSelectionBtns: React.FC<ViewSelectionBtnsProps> = ({
+  currentView,
+  setCurrentView,
+}) => {
   return (
     <View className="flex flex-row mt-2 gap-2">
-      <Btns text="Tasks" />
-      <Btns text="Habits" />
-      <Btns text="Routine" />
+      <Btns
+        text="Tasks"
+        active={currentView === "tasks"}
+        setCurrentView={setCurrentView}
+        changeTo="tasks"
+      />
+      <Btns
+        text="Habits"
+        active={currentView === "habits"}
+        setCurrentView={setCurrentView}
+        changeTo="habits"
+      />
+      <Btns
+        text="Routine"
+        active={currentView === "routine"}
+        setCurrentView={setCurrentView}
+        changeTo="routine"
+      />
     </View>
   );
 };
 
-const Btns: React.FC<{ text: string }> = ({ text }) => {
+interface BtnsProps {
+  text: string;
+  active: boolean;
+  setCurrentView: React.Dispatch<React.SetStateAction<string>>;
+  changeTo: string;
+}
+
+const Btns: React.FC<BtnsProps> = ({
+  text,
+  active,
+  setCurrentView,
+  changeTo,
+}) => {
+  const onPress = () => {
+    setCurrentView(changeTo);
+  };
+
   return (
-    <TouchableHighlight className="bg-[#FDFDFD] px-4 h-[45px] mt-4">
+    <TouchableHighlight
+      className={`bg-[#FDFDFD] px-4 h-[45px] mt-4 ${
+        active ? "border-b-2 border-blue-200" : ""
+      }`}
+      underlayColor="#FFFFFF"
+      onPress={onPress}
+    >
       <Text className="text-black text-xl font-semibold mx-auto my-auto">
         {text}
       </Text>
