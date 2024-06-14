2f0732f91fdd1ff122e1d902c845b9d04e000a2b
youssefr26 <ryoussef5087@gmail.com>
2024-06-14 13:30:29 -0400

    Created a page to display account info

--- lifescape-app/app/(app)/(tabs)/overview/account/index.tsx
+++ lifescape-app/app/(app)/(tabs)/overview/account/index.tsx
@@ -1,27 +1,41 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text} from 'react-native';
 import React from 'react';
+import 'nativewind'
+
+import { useGlobalContext } from '@/context/GlobalProvider';
 
 const Account = () => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Account Details</Text>
-      {"More details"}
-    </View>
-  );
-};
+  //const {user} = useGlobalContext();
+  const user = {
+    username: 'Ramzey',
+    email: 'RY@gmail',
+    emailVerified: true,
+  };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 20,
-  },
-  text: {
-    textAlign: 'center',
-    fontSize: 20,
-    marginVertical: 20,
-  },
-});
+  return (
+    <View className="flex-1 justify-center items-center p-5 bg-gray-100">
+    <View className="w-full max-w-md bg-white p-6 rounded-lg shadow-md">
+      <Text className="text-center text-2xl font-bold my-5">Account Details</Text>
+      
+      <View className="my-3">
+        <Text className="text-lg font-semibold">Username:</Text>
+        <Text className="text-base">{user.username}</Text>
+      </View>
+
+      <View className="my-3">
+        <Text className="text-lg font-semibold">Email:</Text>
+        <Text className="text-base">{user.email}</Text>
+      </View>
+
+      <View className="my-3">
+        <Text className="text-lg font-semibold">Email Verification:</Text>
+        <Text className={`text-base ${user.emailVerified ? 'text-green-600' : 'text-red-600'}`}>
+          {user.emailVerified ? 'Verified' : 'Not Verified'}
+        </Text>
+      </View>
+    </View>
+  </View>
+);
+};
 
 export default Account;
