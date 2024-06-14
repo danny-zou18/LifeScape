b53cadef01e984c489647b1daad2c38a64887590
dannyzou18 <dzou3814@gmail.com>
2024-06-14 17:08:18 -0400

    made a btn to open

--- lifescape-app/components/home/HabitWrapper.tsx
+++ lifescape-app/components/home/HabitWrapper.tsx
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
+import CreateHabitBtn from '../habits/CreateHabitBtn'
+
 const HabitWrapper = () => {
   return (
     <View >
-      <Text>HabitWrapper</Text>
+      <CreateHabitBtn />
     </View>
   )
 }
--- lifescape-app/components/home/TaskWrapper.tsx
+++ lifescape-app/components/home/TaskWrapper.tsx
@@ -1,16 +1,11 @@
 import { View } from "react-native";
 import React from "react";
 
-import { useTaskContext } from "@/context/TaskProvider";
-
 import CreateTaskBtn from "../tasks/CreateTaskBtn";
 import TaskCreationModal from "../tasks/TaskCreationModal";
 import DisplayTasks from "../tasks/DisplayTasks";
 
 const TaskWrapper: React.FC = () => {
-  const { tasks, setTasks, taskCreationOpen, setTaskCreationOpen } =
-    useTaskContext();
-
   return (
     <View>
       <TaskCreationModal />
