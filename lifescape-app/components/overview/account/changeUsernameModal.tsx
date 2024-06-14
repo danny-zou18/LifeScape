f979a32b86612843e36073d3e6ba66e1a4cce812
jkska23 <jkska23@gmail.com>
2024-06-14 15:12:41 -0400

    added modal

---
+++ lifescape-app/components/overview/account/changeUsernameModal.tsx
@@ -0,0 +1,11 @@
+import {Text, View, Modal, Button, TextInput, SafeAreaView} from 'react-native';
+
+const ChangeUsernameModal = () => {
+    return (
+        <Modal>
+            <SafeAreaView>
+                
+            </SafeAreaView>
+        </Modal>
+    );
+}
