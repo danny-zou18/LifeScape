88b075274cb49f09a3bd2b83a6f5e825fca23e89
dannyzou18 <dzou3814@gmail.com>
2024-07-04 00:23:25 -0400

    added defense icon

--- lifescape-app/components/home/RewardsManager.tsx
+++ lifescape-app/components/home/RewardsManager.tsx
@@ -59,6 +59,16 @@ const RewardsManager = () => {
           </View>
           <Text className="text-white">2</Text>
         </View>
+        <View className="h-full flex flex-row items-center gap-1">
+          <View className="h-full">
+            <Image
+              source={require("../../assets/images/rewardicons/defense.png")}
+              resizeMode="contain"
+              style={{ flex: 1, aspectRatio: 1 }}
+            />
+          </View>
+          <Text className="text-white">2</Text>
+        </View>
       </View>
     </View>
   );
