ea9e92d13533855835cadee93ab2a6e484e0b812
dannyzou18 <dzou3814@gmail.com>
2024-07-06 19:23:09 -0400

    added streak check

--- lifescape-app/context/HabitProvider.tsx
+++ lifescape-app/context/HabitProvider.tsx
@@ -59,6 +59,22 @@ const HabitProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
   useEffect(() => {
     const fetchHabits = async () => {
       try {
+        const response = await api.put(`/habits/checkStreaks/${user.uid}/${userCharacter.id}`, {}, {
+          headers: {
+            Authorization: await user.getIdToken(),
+          },
+        });
+        if (response.status === 200) {
+          console.log("Streaks checked");
+        }
+      } catch (error) {
+        if (isAxiosError(error)) {
+          console.log(error.response?.data);
+        } else {
+          console.log(error);
+        }
+      }
+      try {
         const response = await api.get(
           `/habits/get/${user.uid}/${userCharacter.id}`,
           {
