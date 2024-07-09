1e957347a662a9ebd06fe3417242655a9d5b7ff9
dannyzou18 <dzou3814@gmail.com>
2024-07-09 19:51:06 -0400

    fixed daily routine styling, displayed nicely now.

--- lifescape-app/components/routine/DisplayTodaysRoutine.tsx
+++ lifescape-app/components/routine/DisplayTodaysRoutine.tsx
@@ -1,47 +1,9 @@
-import { ScrollView, StyleSheet } from "react-native";
 import React from "react";
 import { Calendar } from "react-native-big-calendar";
 
 import IndividualRoutines from "./IndividualRoutines";
 
 import { useRoutineContext } from "@/context/RoutineProvider";
-
-const events = [
-  {
-    title: "Meeting",
-    start: new Date(
-      new Date().getFullYear(),
-      new Date().getMonth(),
-      new Date().getDate(),
-      14,
-      0
-    ),
-    end: new Date(
-      new Date().getFullYear(),
-      new Date().getMonth(),
-      new Date().getDate(),
-      16,
-      0
-    ),
-  },
-  {
-    title: "Coffee break",
-    start: new Date(
-      new Date().getFullYear(),
-      new Date().getMonth(),
-      new Date().getDate(),
-      15,
-      0
-    ),
-    end: new Date(
-      new Date().getFullYear(),
-      new Date().getMonth(),
-      new Date().getDate(),
-      16,
-      0
-    ),
-  },
-];
 
 const DisplayTodaysRoutine = () => {
   const { todaysRoutine } = useRoutineContext();
@@ -55,7 +17,6 @@ const DisplayTodaysRoutine = () => {
       swipeEnabled={false}
       dayHeaderStyle={{ display: "none" }}
       headerContainerStyle={{display: "none"}}
-      
     />
   );
 };
--- lifescape-app/components/routine/TopBar.tsx
+++ lifescape-app/components/routine/TopBar.tsx
@@ -5,7 +5,7 @@ import CreateTaskBtn from './CreateRoutineBtn'
 
 const TopBar = () => {
   return (
-    <View className='flex flex-row justify-between items-center pl-2 pr-1 '>
+    <View className='flex flex-row justify-between items-center pl-2 pr-1 mb-4 '>
         <Text>Filler</Text>
         <CreateTaskBtn />
     </View>
