19898a10375548937b54f060b013fe03d4712956
dannyzou18 <dzou3814@gmail.com>
2024-07-09 21:29:22 -0400

    routine deletion works

--- lifescape-app/components/routine/IndividualRoutines.tsx
+++ lifescape-app/components/routine/IndividualRoutines.tsx
@@ -2,23 +2,52 @@ import { View, Text, TouchableOpacity, Alert } from "react-native";
 import React from "react";
 import { EventRenderer } from "react-native-big-calendar";
 import { useRoutineContext, CustomEventType } from "@/context/RoutineProvider";
+import api from "@/api/axios";
+import { isAxiosError } from "axios";
+
+import { useGlobalContext } from "@/context/GlobalProvider";
 
 const IndividualRoutines: EventRenderer<CustomEventType> = (
   event,
   touchableOpacityProps
 ) => {
-  const { setEditRoutineOpen, setCurrentEditRoutine } = useRoutineContext();
+  const { user } = useGlobalContext();
+  const { setEditRoutineOpen, setCurrentEditRoutine, setTodaysRoutine } = useRoutineContext();
 
   const onPressEvent = () => {
     setCurrentEditRoutine(event.routine);
     setEditRoutineOpen(true);
   };
 
-  
+  const handleDeleteRoutine = async () => {
+    try {
+      const response = await api.delete(
+        `/routine/delete/${user.uid}/${event.routine.id}`,
+        {
+          headers: {
+            Authorization: await user.getIdToken(),
+          },
+        }
+      );
+      if (response.status === 200) {
+        console.log("Routine deleted successfully");
+        setTodaysRoutine((prev) =>
+          prev.filter((r) => r.routine.id !== event.routine.id)
+        );
+      }
+    } catch(error) {
+      if (isAxiosError(error)) {
+        console.log(error.response?.data);
+      } else {
+        console.log(error);
+      }
+    }
+  }
+
 
   const confirmDeletionAlert = () => {
     Alert.alert("Delete this routine?", "This action cannot be undone.", [
-      { text: "Delete", onPress: () => console.log("Delete Pressed") },
+      { text: "Delete", onPress: () => handleDeleteRoutine() },
       {
         text: "Cancel",
         onPress: () => console.log("Cancel Pressed"),
