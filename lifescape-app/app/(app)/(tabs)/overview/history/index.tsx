18f396cf09167471abce1a2349841d7ad8dbfb26
dannyzou18 <dzou3814@gmail.com>
2024-07-12 15:49:57 -0400

    copied from branch#38
    Co-authored-by: Ramzey Youssef <ryoussef5087@gmail.com>

---
+++ lifescape-app/app/(app)/(tabs)/overview/history/index.tsx
@@ -0,0 +1,37 @@
+import { View, Text} from 'react-native';
+import React from 'react';
+import 'nativewind'
+
+import { useGlobalContext } from '@/context/GlobalProvider';
+
+const History = () => {
+  const {user, userCharacter} = useGlobalContext();
+
+  const taskCount = userCharacter.Tasks ? userCharacter.Tasks.length : 0;
+  const habitCount = userCharacter.Habits ? userCharacter.Habits.length : 0;
+  const routineCount = userCharacter.Routine ? userCharacter.Routine.length : 0;
+
+
+  return (
+    <View className="flex-1 bg-gray-100 p-5">
+      <View className="w-full bg-white p-6 rounded-lg shadow-md">
+        <View className="my-3">
+          <Text className="text-lg font-semibold">Number of Tasks:</Text>
+          <Text className="text-base">{taskCount}</Text>
+        </View>
+
+        <View className="my-3">
+          <Text className="text-lg font-semibold">Number of Habits:</Text>
+          <Text className="text-base">{habitCount}</Text>
+        </View>
+
+        <View className="my-3">
+          <Text className="text-lg font-semibold">Number of Routines:</Text>
+          <Text className= "text-base">{routineCount}</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default History;
