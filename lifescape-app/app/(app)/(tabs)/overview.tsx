bf4565116daa2594a5933f8db5e887e1a243a06e
danny-zou18 <121741891+danny-zou18@users.noreply.github.com>
2024-07-12 14:51:06 -0400

    Revert "39 create a history page"

--- lifescape-app/app/(app)/(tabs)/overview.tsx
+++ lifescape-app/app/(app)/(tabs)/overview.tsx
@@ -1,10 +1,10 @@
-import { View, Text, ActivityIndicator, Button, StyleSheet } from "react-native";
+import { View, Text, ActivityIndicator, Button } from "react-native";
 import React, { useState } from "react";
 
 import { FIREBASE_AUTH } from "@/FirebaseConfig";
 import { signOut } from "firebase/auth";
 
-import { useRouter, Link } from "expo-router";
+import { useRouter } from "expo-router";
 
 import { useGlobalContext } from "@/context/GlobalProvider";
 
@@ -34,28 +34,21 @@ const Overview = () => {
         setIsSigningIn(false);
       });
   };
-  
+
   return (
-    <View className="flex-1 justify-between p-5 bg-gray-100">
-      <View className="flex-col items-center mb-5">
-        <Link href="/overview/account" className="px-5 py-3 bg-black rounded mb-3 w-40">
-          <Text className="text-white font-bold">Account</Text>
-        </Link>
-        <Link href="/overview/history" className="px-5 py-3 bg-black rounded mb-3 w-40">
-          <Text className="text-white font-bold">History</Text>
-        </Link>
-      </View>
-      <Text className="text-center text-xl my-5">Overview</Text>
+    <View>
+      <Text>Overview</Text>
       {isSigningIn ? (
         <ActivityIndicator size="large" color="#0000ff" />
       ) : (
-        <View className="items-center">
+        <>
           <Button
             title="Sign Out"
-            onPress={handleSignOut}
-            color="#000000"
-          />
-        </View>
+            onPress={() => {
+              handleSignOut();
+            }}
+          ></Button>
+        </>
       )}
     </View>
   );
