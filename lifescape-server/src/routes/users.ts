c8e964b4ba7c95cb92541eeebeaf9985689d4736
dannyzou18 <dzou3814@gmail.com>
2024-07-12 16:24:01 -0400

    created endpoint for fetching psql user data

--- lifescape-server/src/routes/users.ts
+++ lifescape-server/src/routes/users.ts
@@ -8,14 +8,14 @@ const router = express.Router();
 /** @api {post} /users/register Register User
  * @apiName RegisterUser
  * @apiGroup User
- * 
+ *
  * @apiDescription Register a user
- * 
+ *
  * @apiBody {String} name Name of the user
  * @apiBody {String} username Username of the user
  * @apiBody {String} email Email of the user
  * @apiBody {String} password Password of the user
- * 
+ *
  * @apiSuccess {String} message Success message
  * @apiSuccess {Boolean} success Success status
  * @apiSuccess {Object} newUser New user object
@@ -43,7 +43,7 @@ const router = express.Router();
  *   HTTP/1.1 400 Bad Request
  *   {
  *     "error": "User Creation Failed"
- *   } 
+ *   }
  */
 router.post("/register", async (req, res) => {
   const { name, username, email, password } = req.body;
@@ -95,6 +95,55 @@ router.post("/register", async (req, res) => {
       console.log(error);
       return res.status(400).json({ error: "User Creation Failed" });
     });
+});
+
+/** @api {get} /users/:id Get User
+ *  @apiName GetUser
+ *  @apiGroup User
+ *
+ *  @apiDescription Get a user by ID
+ *
+ *  @apiParam {String} id User ID
+ *
+ *  @apiSuccess {String} message Success message
+ *  @apiSuccess {Boolean} success Success status
+ *  @apiSuccess {Object} user User object
+ *  @apiSuccessExample {json} Success-Response:
+ *      HTTP/1.1 200 OK
+ *      {
+ *        "message": "User Found",
+ *        "success": true,
+ *        "user": USEROBJECT
+ *      }
+ *  @apiError UserNotFound User not found
+ *  @apiErrorExample {json} UserNotFound:
+ *      HTTP/1.1 404 Not Found
+ *      {
+ *        "error": "User not found"
+ *      }
+ *  @apiError UserFetchFailed User fetch failed
+ *  @apiErrorExample {json} UserFetchFailed:
+ *      HTTP/1.1 400 Bad Request
+ *      {
+ *        "error": "User Fetch Failed"
+ *      }
+ */
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const user = await db.users
+    .findUnique({
+      where: {
+        id: id,
+      },
+    })
+    .catch((error) => {
+      console.log(error);
+      return res.status(400).json({ error: "User Fetch Failed" });
+    });
+  if (!user) {
+    return res.status(404).json({ error: "User not found" });
+  }
+  return res.status(200).json({ message: "User Found", success: true, user });
 });
 
 export { router as userRouter };
