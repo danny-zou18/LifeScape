2915b6a84819943a90aed9a5e07e07d41210d0cd
dannyzou18 <dzou3814@gmail.com>
2024-07-15 15:25:16 -0400

    moved most of stuff in overview to overview/settings

--- lifescape-app/app/(app)/(tabs)/overview/index.tsx
+++ lifescape-app/app/(app)/(tabs)/overview/index.tsx
@@ -1,76 +1,13 @@
 import {
   SafeAreaView,
-  View,
-  Text,
-  ActivityIndicator,
-  Button,
 } from "react-native";
-import React, { useState } from "react";
-
-import { FIREBASE_AUTH } from "@/FirebaseConfig";
-import { signOut } from "firebase/auth";
-
-import { useRouter, Link } from "expo-router";
-
-import { useGlobalContext } from "@/context/GlobalProvider";
-
-import AntDesign from "@expo/vector-icons/AntDesign";
+import React from "react";
 
 const Overview = () => {
-  const [isSigningIn, setIsSigningIn] = useState<boolean>(false);
-
-  const { loggedIn, setLoggedIn } = useGlobalContext();
-
-  const router = useRouter();
-
-  const auth = FIREBASE_AUTH;
-
-  const handleSignOut = async () => {
-    setIsSigningIn(true);
-    await signOut(auth)
-      .then(() => {
-        if (loggedIn) {
-          setLoggedIn(false);
-        }
-        router.replace("sign-in");
-        alert("User Logged Out");
-      })
-      .catch((error) => {
-        console.log(error);
-      })
-      .finally(() => {
-        setIsSigningIn(false);
-      });
-  };
 
   return (
     <SafeAreaView className=" mx-auto w-[90%] mt-5 mb-2">
-      <View className="w-full rounded-lg overflow-hidden bg-black">
-        <Link href="/overview/account" className="p-4 w-full ">
-          <Text className="text-white">Account</Text>
-        </Link>
-        <View className="w-full h-[1px] bg-white"></View>
-        <Link href="/overview/history" className="p-4  w-full ">
-          <Text className="text-white">History</Text>
-        </Link>
-        <View className="w-full h-[1px] bg-white"></View>
-        <Link href="/overview/settings" className="p-4  w-full ">
-          <Text className="text-white">Settings</Text>
-        </Link>
-      </View>
-
-      {isSigningIn ? (
-        <ActivityIndicator size="large" color="#0000ff" />
-      ) : (
-        <>
-          <Button
-            title="Sign Out"
-            onPress={() => {
-              handleSignOut();
-            }}
-          ></Button>
-        </>
-      )}
+      
     </SafeAreaView>
   );
 };
--- lifescape-app/app/(app)/(tabs)/overview/settings/index.tsx
+++ lifescape-app/app/(app)/(tabs)/overview/settings/index.tsx
@@ -1,12 +1,73 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  SafeAreaView,
+  View,
+  Text,
+  ActivityIndicator,
+  Button,
+} from "react-native";
+import React, { useState } from "react";
+
+import { FIREBASE_AUTH } from "@/FirebaseConfig";
+import { signOut } from "firebase/auth";
+
+import { useRouter, Link } from "expo-router";
+
+import { useGlobalContext } from "@/context/GlobalProvider";
 
 const Settings = () => {
+  const [isSigningIn, setIsSigningIn] = useState<boolean>(false);
+
+  const { loggedIn, setLoggedIn } = useGlobalContext();
+
+  const router = useRouter();
+
+  const auth = FIREBASE_AUTH;
+
+  const handleSignOut = async () => {
+    setIsSigningIn(true);
+    await signOut(auth)
+      .then(() => {
+        if (loggedIn) {
+          setLoggedIn(false);
+        }
+        router.replace("sign-in");
+        alert("User Logged Out");
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setIsSigningIn(false);
+      });
+  };
+
   return (
-    <View>
-      <Text>Settings</Text>
-    </View>
-  )
-}
+    <SafeAreaView className=" mx-auto w-[90%] mt-5 mb-2">
+      <View className="w-full rounded-lg overflow-hidden bg-black">
+        <Link href="/overview/account" className="p-4 w-full ">
+          <Text className="text-white">Account</Text>
+        </Link>
+        <View className="w-full h-[1px] bg-white"></View>
+        <Link href="/overview/history" className="p-4  w-full ">
+          <Text className="text-white">History</Text>
+        </Link>
+        <View className="w-full h-[1px] bg-white"></View>
+      </View>
+
+      {isSigningIn ? (
+        <ActivityIndicator size="large" color="#0000ff" />
+      ) : (
+        <>
+          <Button
+            title="Sign Out"
+            onPress={() => {
+              handleSignOut();
+            }}
+          ></Button>
+        </>
+      )}
+    </SafeAreaView>
+  );
+};
 
 export default Settings;
