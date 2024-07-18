c0c34851af89962b1a9933b22d4d712c215250d2
dannyzou18 <dzou3814@gmail.com>
2024-07-18 19:21:28 -0400

    moved account methods section of account screen to its own component file, also created login methods

---
+++ lifescape-app/components/overview/settings/account/LoginMethod.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const LoginMethod = () => {
+  return (
+    <View>
+      <Text className="font-[600] mb-1 ml-2">LOGIN METHODS</Text>
+    </View>
+  )
+}
+
+export default LoginMethod
