c1ed57bfddb26845f99592866801d80c67c227ea
dannyzou18 <dzou3814@gmail.com>
2024-07-19 15:13:29 -0400

    work on desktop

--- lifescape-app/components/character/characterOverview/ImageXpBar.tsx
+++ lifescape-app/components/character/characterOverview/ImageXpBar.tsx
@@ -27,18 +27,23 @@ const ImageXpBar = () => {
           uri: "https://media1.tenor.com/m/sepKXIenuG0AAAAC/cat-meme-cat-meme-face.gif",
         }}
       />
-      <View
-        className="flex flex-row bg-transparent w-full h-4 mt-2"
-      >
-        <View className="rounded-full bg-black px-[0.4rem] flex justify-center items-center w-[7%]">
-          <Text className="text-xs text-white">{userCharacter?.level}</Text>
-        </View>
-        <View className="ml-1 min-w-[92%] overflow-hidden rounded-full bg-[#00000090]">
+      <View className="mt-1 flex h-2 w-full flex-row bg-transparent">
+        <View className="min-w-[100%] overflow-hidden rounded-full bg-[#00000090]">
           <View
             style={{ width: expPercentage }}
             className={`flex h-full items-center justify-center rounded-l-full rounded-r-lg bg-green-400`}
           ></View>
         </View>
+      </View>
+      <View className="flex flex-row justify-between">
+        <Text>Level {userCharacter?.level}</Text>
+        <Text>
+          {userCharacter?.experience.toFixed(2)} /{" "}
+          {(
+            0.8 * Math.pow(userCharacter?.level ?? 0, 2) +
+            2 * (userCharacter?.level ?? 0)
+          ).toFixed(2)}
+        </Text>
       </View>
     </View>
   );
