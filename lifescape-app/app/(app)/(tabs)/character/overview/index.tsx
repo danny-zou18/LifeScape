82902184384bfa43da3715b19b11b6ca97197768
dannyzou18 <dzou3814@gmail.com>
2024-07-23 14:07:15 -0400

    displayed character's class and subclass in character overview

--- lifescape-app/app/(app)/(tabs)/character/overview/index.tsx
+++ lifescape-app/app/(app)/(tabs)/character/overview/index.tsx
@@ -2,11 +2,13 @@ import { View } from "react-native";
 import React from "react";
 
 import ImageXpBar from "@/components/character/characterOverview/ImageXpBar";
+import GeneralInfo from "@/components/character/characterOverview/GeneralInfo";
 
 const CharacterOverview = () => {
   return (
-    <View className="mx-auto w-[90%] py-2">
+    <View className="mx-auto w-[90%] py-2 flex flex-col">
       <ImageXpBar />
+      <GeneralInfo />
     </View>
   );
 };
--- lifescape-app/components/character/characterOverview/GeneralInfo.tsx
+++ lifescape-app/components/character/characterOverview/GeneralInfo.tsx
@@ -1,12 +1,22 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
+
+import { useGlobalContext } from "@/context/GlobalProvider";
 
 const GeneralInfo = () => {
+  const { userCharacter } = useGlobalContext();
   return (
-    <View>
-      <Text>GeneralInfo</Text>
+    <View className="mt-12 rounded-md border-[1px] px-2">
+      <View className="border-b-[1px] py-2 flex flex-row justify-between">
+        <Text>Class:</Text> 
+        <Text>{userCharacter?.class}</Text>
+      </View>
+      <View className="py-2 flex flex-row justify-between">
+        <Text>Subclass:</Text>
+        <Text>{userCharacter?.subclass}</Text>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default GeneralInfo
+export default GeneralInfo;
