3c44630ffaad307a022c3add863b54c1f80bd423
dannyzou18 <dzou3814@gmail.com>
2024-08-02 15:09:20 -0400

    did some work

--- lifescape-app/app/(app)/(tabs)/overview/_layout.tsx
+++ lifescape-app/app/(app)/(tabs)/overview/_layout.tsx
@@ -1,66 +1,66 @@
-import React from 'react';
-import { Stack, Link, usePathname } from 'expo-router';
+import { View, Text, Image } from "react-native";
+import React from "react";
+import { Stack, Link, usePathname } from "expo-router";
 
-import Header from '@/components/overview/Header';
+import Header from "@/components/overview/Header";
 
 export default function OverviewLayout() {
-
   const pathName = usePathname();
 
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen 
-        name="index" 
-        options={{ 
+      <Stack.Screen
+        name="index"
+        options={{
           headerShown: true,
           title: "Overview",
           header: Header,
-        }} 
+        }}
       />
-      <Stack.Screen 
-        name="settings" 
+      <Stack.Screen
+        name="settings"
         options={{
           headerShown: true,
           title: "Settings",
           headerBackTitle: "Done",
           presentation: "modal",
-          headerRight: () => (
-            <Link
-              href={"/overview"}
-            >
-              Done
-            </Link>
-          ),
-          headerLeft: pathName !== "/overview/settings" ? (() => (
-            <Link
-              href={"/overview/settings"}
-            >
-              Back
-            </Link>
-          )) : undefined,
+          headerRight: () => <Link href={"/overview"}>Done</Link>,
+          headerLeft:
+            pathName !== "/overview/settings"
+              ? () => <Link href={"/overview/settings"}>Back</Link>
+              : undefined,
         }}
       />
-      <Stack.Screen 
-        name="friends" 
+      <Stack.Screen
+        name="friends"
         options={{
           headerShown: true,
           title: "Friends",
           headerBackTitle: "Done",
           presentation: "modal",
-          headerRight: () => (
-            <Link
-              href={"/overview"}
-            >
-              Done
-            </Link>
-          ),
-          headerLeft: pathName !== "/overview/friends" ? (() => (
-            <Link
-              href={"/overview/friends"}
-            >
-              Back
-            </Link>
-          )) : undefined,
+          headerLeft: () => <Link href={"/overview"}>Done</Link>,
+          headerRight:
+            pathName !== "/overview/friends"
+              ? () => <Link href={"/overview/friends"}>Back</Link>
+              : () => (
+                  <View className="h-10 w-32 flex flex-row items-center">
+                    <Link
+                      href="/overview/friends/add_friend"
+                      className="h-10 items-center py-3 mr-1"
+                    >
+                      <Text>Add Friend</Text>
+                    </Link>
+                    <View className="h-[80%] w-full">
+                      <View className="h-full w-full">
+                        <Image
+                          source={require("../../../../assets/images/add_friend.png")}
+                          resizeMode="contain"
+                          style={{ flex: 1, aspectRatio: 1 }}
+                        />
+                      </View>
+                    </View>
+                  </View>
+                ),
         }}
       />
     </Stack>
--- lifescape-app/app/(app)/(tabs)/overview/friends/index.tsx
+++ lifescape-app/app/(app)/(tabs)/overview/friends/index.tsx
@@ -1,15 +1,13 @@
-import { View, Text, Image } from 'react-native'
-import { Link } from 'expo-router'
-import React from 'react'
+import { View, Text, Image } from "react-native";
+import { Link } from "expo-router";
+import React from "react";
 
 const Friends = () => {
   return (
     <View>
-      <Link href="/overview/friends/add_friend" className="p-4 w-full ">
-          <Text className="text-white">Add Friend</Text>
-        </Link>
+      
     </View>
-  )
-}
+  );
+};
 
-export default Friends
+export default Friends;
