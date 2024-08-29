4480db106d52fe52e50a982ae5c68449d448244a
dannyzou18 <dzou3814@gmail.com>
2024-08-29 19:35:26 -0400

    added individual friends page to layout

---
+++ lifescape-app/app/(app)/(tabs)/overview/friends/friend/[friendId].tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const FriendOverview = () => {
+  return (
+    <View>
+      <Text>FriendOverview</Text>
+    </View>
+  )
+}
+
+export default FriendOverview
