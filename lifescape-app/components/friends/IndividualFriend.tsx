443b78ee615684c88f0d653d5acd99ea6270d0e9
dannyzou18 <dzou3814@gmail.com>
2024-08-30 18:12:59 -0400

    got initial friends overview page to work

--- lifescape-app/components/friends/IndividualFriend.tsx
+++ lifescape-app/components/friends/IndividualFriend.tsx
@@ -1,17 +1,26 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Friendship } from '@/types/db_types'
+import { View, Text, TouchableHighlight } from "react-native";
+import React from "react";
+import { router, useSegments, Link } from "expo-router";
+import { Friendship } from "@/types/db_types";
 
 interface IndividualFriendProps {
-    friendData: Friendship
+  friendData: Friendship;
 }
 
-const IndividualFriend: React.FC<IndividualFriendProps> = ({friendData}) => {
+const IndividualFriend: React.FC<IndividualFriendProps> = ({ friendData }) => {
+  const segments = useSegments();
+  const onPress = () => {
+    router.replace(`/overview/friends/friend/${friendData.user_id}`);
+    console.log(segments.join("/"));
+  };
+
   return (
-    <View className='bg-blue-400 p-2 rounded-md'>
+    <View className="bg-blue-400 rounded-md">
+      <Link className="p-4 w-full h-full" href={`/overview/friends/friend/${friendData.user_id}`}>
         <Text>{friendData.user_username}</Text>
+      </Link>
     </View>
-  )
-}
+  );
+};
 
-export default IndividualFriend
+export default IndividualFriend;
