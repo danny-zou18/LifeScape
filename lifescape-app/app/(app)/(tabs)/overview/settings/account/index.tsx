210b623a17cc4aae884c5d6f6c971ba32411fff6
Benjamin Lee <benjaminlee755@gmail.com>
2024-11-08 17:17:12 -0500

    added an Account Action section to the settings page

--- lifescape-app/app/(app)/(tabs)/overview/settings/account/index.tsx
+++ lifescape-app/app/(app)/(tabs)/overview/settings/account/index.tsx
@@ -3,12 +3,14 @@ import React from "react";
 
 import AccountInfo from "@/components/overview/settings/account/AccountInfo";
 import LoginMethod from "@/components/overview/settings/account/LoginMethod";
+import AccountAction from "@/components/overview/settings/account/AccountAction";
 
 const Account = () => {
   return (
     <View className="flex-1 gap-12 bg-gray-100 p-5">
       <AccountInfo />
       <LoginMethod />
+      <AccountAction />
     </View>
   );
 };
